9ba2deb4dec617f80c0ce7b8c5b1c02493c26953
Rupa261003 <143867409+Rupa261003@users.noreply.github.com>
2023-09-09 10:10:05 +0530

    Create DefaultDemo.java

---
+++ DefaultDemo.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+ class Sample{
+    void display()
+    {
+    System.out.println("Hello World");
+    }
+ }
+public class DefaultDemo{
+
+    public static void main(String args[]){
+        Sample s=new Sample();
+        s.display();
+    }
+}
