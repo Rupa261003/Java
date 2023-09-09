eca3c20735a2501f93d4c4f21837e1a0700d6f3c
Rupa261003 <143867409+Rupa261003@users.noreply.github.com>
2023-09-09 09:35:32 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Student {
+    public static void main(String args[]){
+    String sname="Rupa";
+    double avg= 9.15;
+    
+        
+        System.out.println( sname+ " has an avg is " +avg);
+    }
+}
